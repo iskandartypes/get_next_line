9dc579addebf561e633199ed5181f6907b65df51
naregjan <iskandar.kourkjian.mowad@gmail.com>
2019-03-25 02:43:17 -0700

    fixed absolute path to lib in header

--- inc/get_next_line.h
+++ inc/get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: ikourkji <ikourkji@student.42.us.or>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/08 02:00:01 by ikourkji          #+#    #+#             */
-/*   Updated: 2019/01/09 04:47:11 by ikourkji         ###   ########.fr       */
+/*   Updated: 2019/03/25 02:43:04 by ikourkji         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include "libft/libft.h"
+# include "libft.h"
 
 # define BUFF_SIZE 32
 # define CHECK(x) if (x) return (-1);
